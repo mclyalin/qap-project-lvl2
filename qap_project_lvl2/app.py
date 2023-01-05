b914d035acdee175b34c696d7a1cdc9247f111c9
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-05 21:43:37 +0500

    Fix price output

--- qap_project_lvl2/app.py
+++ qap_project_lvl2/app.py
@@ -32,7 +32,7 @@ def convert(message: telebot.types.Message):
   except Exception as e:
     bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
   else:
-    text = f'Цена {amount} {quote} в {base} = {price}'
+    text = f'Цена {amount} {quote} в {base} = {"{0:.2f}".format(price)}'
     bot.send_message(message.chat.id, text)
 
 bot.polling()
