4473ea0d7cc3fbfe3da1290d35c978a913fb3f4c
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-05 17:16:45 +0500

    Refactor exceptions handling

--- qap_project_lvl2/app.py
+++ qap_project_lvl2/app.py
@@ -22,15 +22,20 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-  values = message.text.split(' ')
+  try:
+    values = message.text.split(' ')
 
-  if len(values) != 3:
-    raise ConvertionException('Неверное количество параметров')
+    if len(values) != 3:
+      raise ConvertionException('Неверное количество параметров')
 
-  quote, base, amount = values
-  quote_ticker, base_ticker, total_base = Converter.convert(quote, base, amount)
-  ##
-  text = f'Цена {amount} {quote_ticker} в {base_ticker} = {total_base}'
-  bot.send_message(message.chat.id, text)
+    quote, base, amount = values
+    quote_ticker, base_ticker, total_base = Converter.convert(quote, base, amount)
+  except ConvertionException as e:
+    bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+  except Exception as e:
+    bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
+  else:
+    text = f'Цена {amount} {quote_ticker} в {base_ticker} = {total_base}'
+    bot.send_message(message.chat.id, text)
 
 bot.polling()
