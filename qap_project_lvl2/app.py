0460690027bb4d181b84042667b6689c37fd80d6
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-05 16:19:11 +0500

    Add converter class

--- qap_project_lvl2/app.py
+++ qap_project_lvl2/app.py
@@ -13,9 +13,39 @@ keys = {
   'доллар': 'USD'
 }
 
+class ConvertionException(Exception):
+  pass
+
+class Converter:
+  @staticmethod
+  def convert(quote: str, base: str, amount: str):
+    if quote == base:
+      raise ConvertionException(f'Невозможно перевести {quote} в {base}')
+
+    if quote not in keys:
+      raise ConvertionException(f'Неудалось обработать валюту {quote}')
+
+    if base not in keys:
+      raise ConvertionException(f'Неудалось обработать валюту {base}')
+
+    quote_ticker, base_ticker = keys[quote], keys[base]
+
+    try:
+      amount = float(amount)
+    except ValueError:
+      raise ConvertionException(f'Неудалось обработать количество {amount}')
+
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+    rate = int(json.loads(r.content)[keys[base]])
+    total_base = amount * rate
+
+    return quote_ticker, base_ticker, total_base
+
+
+
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-  text = 'Чтобы начать работу введите комманду в следующем формате:\n \
+  text = 'Введите комманду в следующем формате:\n \
 <из какой валюты> <в какую валюту> <сколько перевести>\n \
 Например: доллар евро 100\n \
 /values - список всех доступных валют'
@@ -30,10 +60,15 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-  quote, base, amount = message.text.split(' ')
-  r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
-  total_base = json.loads(r.content)[keys[base]]
-  text = f'Цена {amount} {keys[quote]} в {keys[base]} = {total_base}'
+  values = message.text.split(' ')
+
+  if len(values) != 3:
+    raise ConvertionException('Неверное количество параметров')
+
+  quote, base, amount = values
+  quote_ticker, base_ticker, total_base = Converter.convert(quote, base, amount)
+  ##
+  text = f'Цена {amount} {quote_ticker} в {base_ticker} = {total_base}'
   bot.send_message(message.chat.id, text)
 
 bot.polling()
